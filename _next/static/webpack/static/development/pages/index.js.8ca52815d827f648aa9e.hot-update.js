fc4eadad56f7139c4ffc8c07bbb239f43ca482d9
Diego-Lap\DiegoLap <dhernandez@grupoeco.com.mx>
2019-08-13 16:18:25 -0700

    primer commit

---
+++ _next/static/webpack/static/development/pages/index.js.8ca52815d827f648aa9e.hot-update.js
@@ -0,0 +1,45 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/Noticias.js":
+/*!********************************!*\
+  !*** ./components/Noticias.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Noticia__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Noticia */ "./components/Noticia.js");
+var _jsxFileName = "C:\\Users\\DiegoLap\\Desktop\\CURSOS\\CURSO UDEMY REACT\\PROYECTOS REACT\\bitcoinapp\\components\\Noticias.js";
+
+ //sfc
+
+var Noticias = function Noticias(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "row",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, props.noticias.map(function (noticia) {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Noticia__WEBPACK_IMPORTED_MODULE_1__["default"], {
+      key: noticia.url,
+      noticia: noticia,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 10
+      },
+      __self: this
+    });
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Noticias);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.8ca52815d827f648aa9e.hot-update.js.map
